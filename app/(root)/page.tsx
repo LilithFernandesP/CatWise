f043f159b1f0f85644200cb52ca77de532a31421
LilithFernandesP <lilith.fernandes@sga.pucminas.br>
2025-08-04 19:25:53 -0300

    cat professor photo

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -37,7 +37,7 @@ const Page = async () => {
                         <Link href='/interview'>Generate a quiz</Link>
                     </Button>
                 </div>
-                <Image src='/robot.png' alt='robo-dude' width={400} height={400} className='max-sm:hidden'/>
+                <Image src='/profCatBg.png' alt='ProfessorCat' width={400} height={400} className='max-sm:hidden rounded-2xl shadow-xl drop-shadow-xl'/>
             </section>
 
             <section className='flex flex-col gap-6 mt-8'>
